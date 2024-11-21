e26db2e9da2dddd70bf6e9a306bb9fadf3c7b075
kirey1412 <kireykayana14@gmail.com>
2024-11-21 20:00:42 +0700

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,48 @@
+import pgzrun
+
+TITLE="Quiz Master"
+WIDTH=870
+HEIGHT=650
+
+marqueebox=Rect(0,0,870,80)
+questionbox=Rect(0,0,650,150)
+timerbox=Rect(0,0,150,150)
+skipbox=Rect(0,0,150,330)
+option1=Rect(0,0,300,150)
+option2=Rect(0,0,300,150)
+option3=Rect(0,0,300,150)
+option4=Rect(0,0,300,150)
+options=[option1,option2,option3,option4]
+
+marquemessage=""
+
+marqueebox.move_ip(0,0)
+questionbox.move_ip(20,100)
+timerbox.move_ip(700,100)
+skipbox.move_ip(700,270)
+option1.move_ip(20,270)
+option2.move_ip(370,270)
+option3.move_ip(20,450)
+option4.move_ip(370,450)
+
+def draw():
+    screen.fill("black")
+    screen.draw.filled_rect(marqueebox,"black")
+    screen.draw.filled_rect(questionbox,"red")
+    screen.draw.filled_rect(timerbox,"green")
+    screen.draw.filled_rect(skipbox,"white")
+    for option in options:
+        screen.draw.filled_rect(option,"yellow")
+    marquemessage="Welcome to the Quiz Master!"
+    screen.draw.textbox(marquemessage,marqueebox,color="red",shadow=(0.5,0.5),scolor="dimgray")
+
+def movemarquee():
+    marqueebox.x-=2
+    if marqueebox.right<0:
+        marqueebox.left=WIDTH
+
+
+def update():
+    movemarquee()
+
+pgzrun.go()
